adbdbde0d8a5cae8435f584a72f41cec423c7435
mina gerges <mgerges@getgroup.com>
2022-04-09 15:10:00 +0200

    init jwt project

---
+++ JwtTest/Helpers/JWT.cs
@@ -0,0 +1,10 @@
+﻿namespace JwtTest.Helpers
+{
+    public class JWT
+    {
+        public string Key { get; set; }
+        public string Assuer { get; set; }
+        public string Audience { get; set; }
+        public string durationInDays { get; set; }
+    }
+}
